48798f8e9a1e0b0a6fb7627f6db64c00e8d7adc7
qt2139 <qt2139@columbia.edu>
2022-09-28 23:50:37 -0400

    update

--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -16,9 +16,9 @@ class ColumbiaStudentResource:
         h = os.environ.get("DBHOST")
 
         conn = pymysql.connect(
-            user=usr,
-            password=pw,
-            host=h,
+            user='admin',
+            password='123456789',
+            host='e6156.cspgkjnwiyzn.us-east-1.rds.amazonaws.com',
             cursorclass=pymysql.cursors.DictCursor,
             autocommit=True
         )
